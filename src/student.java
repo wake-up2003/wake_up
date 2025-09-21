01b0ead11d928541c29123f7fd69449b296c19ab
wake-up2003 <1765901984@qq.com>
2025-09-21 18:50:40 +0800

    第一次提交

--- src/student.java
+++ src/student.java
@@ -2,5 +2,9 @@ public class student {
 
      private  String name;
      private int age;
-     
+
+    public student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }

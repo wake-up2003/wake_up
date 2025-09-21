41c9d3b73be9096b977532fdc5b61470f178b8aa
wake-up2003 <1765901984@qq.com>
2025-09-21 19:14:23 +0800

    更新后

--- src/student.java
+++ src/student.java
@@ -8,8 +8,21 @@ public class student implements  Comparable {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     @Override
     public int compareTo(Object o) {
